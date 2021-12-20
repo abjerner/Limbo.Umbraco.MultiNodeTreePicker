75a4d1301b46913b4a37b469c85ac0a0d273fe86
Anders Bjerner <abjerner@skybrud.dk>
2021-12-20 10:17:29 +0100

    Replaced package.manifest with new BackOfficeJavaScriptAsset class

--- src/Skybrud.Umbraco.MultiNodeTreePicker/Composers/MntpComposer.cs
+++ src/Skybrud.Umbraco.MultiNodeTreePicker/Composers/MntpComposer.cs
@@ -1,6 +1,7 @@
 ﻿using Skybrud.Umbraco.MultiNodeTreePicker.Converters;
 using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Core.WebAssets;
 
 namespace Skybrud.Umbraco.MultiNodeTreePicker.Composers {
     
@@ -8,6 +9,19 @@ namespace Skybrud.Umbraco.MultiNodeTreePicker.Composers {
         
         public void Compose(IUmbracoBuilder builder) {
             builder.WithCollectionBuilder<MntpConverterCollectionBuilder>().Add(() => builder.TypeLoader.GetTypes<IMntpItemConverter>());
+            builder.BackOfficeAssets().Append<BackOfficeJavaScriptAsset>();
+        }
+
+        public class BackOfficeJavaScriptAsset : IAssetFile {
+        
+            public AssetType DependencyType => AssetType.Javascript;
+
+            public string FilePath { get; set; }
+
+            public BackOfficeJavaScriptAsset() {
+                FilePath = "/App_Plugins/Skybrud.Umbraco.MultiNodeTreePicker/MntpConverter.js";
+            }
+
         }
 
     }
