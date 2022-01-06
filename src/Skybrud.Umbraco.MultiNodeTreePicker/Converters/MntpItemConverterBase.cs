ca0383f9adde3d1cc054a378f4f283b89f0c96e4
Anders Bjerner <abjerner@skybrud.dk>
2022-01-06 13:24:13 +0100

    Introduced new "MntpItemConverterBase" class as an alternative to "MntpGenericItemConverter"

---
+++ src/Skybrud.Umbraco.MultiNodeTreePicker/Converters/MntpItemConverterBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Umbraco.Cms.Core.Models.PublishedContent;
+
+namespace Skybrud.Umbraco.MultiNodeTreePicker.Converters {
+
+    public abstract class MntpItemConverterBase<T> : IMntpItemConverter {
+
+        public string Name { get; }
+
+        protected MntpItemConverterBase(string name) {
+            Name = name;
+        }
+
+        public abstract object Convert(IPublishedPropertyType propertyType, IPublishedContent source);
+
+        public virtual Type GetType(IPublishedPropertyType propertyType) {
+            return typeof(T);
+        }
+
+    }
+
+}
