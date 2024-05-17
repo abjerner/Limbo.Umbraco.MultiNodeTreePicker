7c75db92061a03ac52e24dbab772f86b7e74ce29
Anders Bjerner <abjerner@skybrud.dk>
2024-05-17 11:12:56 +0200

    Yes please!

--- debug.bat
+++ debug.bat
@@ -1,2 +1,2 @@
 @echo off
-dotnet build src/Limbo.Umbraco.MultiNodeTreePicker --configuration Debug /t:rebuild /t:pack -p:PackageOutputPath=C:\nuget\Umbraco10
+dotnet build src/Limbo.Umbraco.MultiNodeTreePicker --configuration Debug /t:rebuild /t:pack -p:PackageOutputPath=C:\nuget\Umbraco13
