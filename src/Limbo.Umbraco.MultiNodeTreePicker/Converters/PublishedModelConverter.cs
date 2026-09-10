be5e9c6029e0d83a1b9edf7f2e6730a88ec473ea
Anders Bjerner <abj@limbo.works>
2026-09-10 10:20:44 +0200

    Updated "PublishedModelConverter" to support old and new formats for the "Filter" option
    Umbraco 13 and older version saved the allowed types as a comma separated list of content type aliases, whereas Umbraco 17 instead saves the content type keys.
    
    I haven't tested whether the Umbraco 17 property editor actually still support the aliases, but now at least this converter does. And it supports the new key format.

--- src/Limbo.Umbraco.MultiNodeTreePicker/Converters/PublishedModelConverter.cs
+++ src/Limbo.Umbraco.MultiNodeTreePicker/Converters/PublishedModelConverter.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using Limbo.Umbraco.MultiNodeTreePicker.PropertyEditors;
+using Skybrud.Essentials.Enums;
 using Skybrud.Essentials.Strings;
 using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.Models.PublishedContent;
-using Umbraco.Cms.Core.PropertyEditors;
+using Umbraco.Cms.Core.PublishedCache;
 using Umbraco.Extensions;
 
 #pragma warning disable CS1591
@@ -20,6 +23,7 @@ namespace Limbo.Umbraco.MultiNodeTreePicker.Converters;
 public class PublishedModelConverter : IMntpItemConverter {
 
     private readonly TypeLoader _typeLoader;
+    private readonly IPublishedContentTypeCache _publishedContentTypeCache;
 
     #region Properties
 
@@ -31,8 +35,9 @@ public class PublishedModelConverter : IMntpItemConverter {
 
     #region Constructors
 
-    public PublishedModelConverter(TypeLoader typeLoader) {
+    public PublishedModelConverter(TypeLoader typeLoader, IPublishedContentTypeCache publishedContentTypeCache) {
         _typeLoader = typeLoader;
+        _publishedContentTypeCache = publishedContentTypeCache;
     }
 
     #endregion
@@ -48,16 +53,16 @@ public class PublishedModelConverter : IMntpItemConverter {
     public Type GetType(IPublishedPropertyType propertyType) {
 
         // Ensure the configuration is of the correct type (probably always is)
-        if (propertyType.DataType.ConfigurationObject is not MultiNodePickerConfiguration config) throw new Exception("NOES!");
+        if (propertyType.DataType.ConfigurationObject is not MntpConfiguration config) throw new Exception("NOES!");
 
         // Get the allowed content types from the configuration
-        string[] allowedTypes = StringUtils.ParseStringArray(config.Filter);
+        HashSet<string> allowedTypes = StringUtils.ParseStringSet(config.Filter);
 
         // Set the default type (will be used as fallback
         Type type = typeof(IPublishedContent);
 
         // Determine a common value type if the data type is restricted to one or more types
-        if (allowedTypes.Length > 0 && GetTypeForAllowedTypes(allowedTypes) is { } valueType) type = valueType;
+        if (allowedTypes.Count > 0 && GetTypeForAllowedTypes(config, allowedTypes) is { } valueType) type = valueType;
 
         return type;
 
@@ -69,21 +74,62 @@ public class PublishedModelConverter : IMntpItemConverter {
     /// <see>
     ///     <cref>https://github.com/callumbwhyte/super-value-converters/blob/v9/dev/src/Our.Umbraco.SuperValueConverters/ValueConverters/SuperValueConverterBase.cs#L61</cref>
     /// </see>
-    private Type? GetTypeForAllowedTypes(string[] allowedTypes) {
+    private Type? GetTypeForAllowedTypes(MntpConfiguration config, HashSet<string> allowedTypes) {
 
         // Calculate a list of all the allowed model types
-        IEnumerable<Type> types = _typeLoader.GetTypes<IPublishedElement>().Where(type => {
-            string modelName = type.GetCustomAttribute<PublishedModelAttribute>(false)?.ContentTypeAlias ?? type.Name;
-            return allowedTypes.InvariantContains(modelName);
-        });
+        IEnumerable<Type> types = GetModelTypes(config, allowedTypes);
 
         // If the data type only has one allowed type, we can return that type right away
-        if (allowedTypes.Length == 1) return types.FirstOrDefault();
+        if (allowedTypes.Count == 1) return types.FirstOrDefault();
 
         return types
             .Select(x => x.GetInterfaces().Where(i => i.IsPublic && i != typeof(IPublishedElement)))
             .IntersectMany()
             .LastOrDefault();
+
+    }
+
+    /// <summary>
+    /// Returns a list of all available model types, based on the object type of the specified <paramref name="config"/> and the set of <paramref name="allowedTypes"/>.
+    /// </summary>
+    /// <param name="config">The MNTP configuration.</param>
+    /// <param name="allowedTypes">A set of the allowed types.</param>
+    /// <returns></returns>
+    private IEnumerable<Type> GetModelTypes(MntpConfiguration config, HashSet<string> allowedTypes) {
+
+        // Parse the object / item type specified in the data type configuration. Ideally we should
+        // always be able to parse this, but if we can't, we just return an empty list of types.
+        if (!EnumUtils.TryParseEnum(config.TreeSource?.ObjectType, out PublishedItemType objectType)) yield break;
+
+        // First criteria is getting all types that implement IPublishedElement, which is the base interface for all
+        // published models. Then we filter those types based on the allowed content types specified in the configuration
+        foreach (Type type in _typeLoader.GetTypes<IPublishedElement>()) {
+
+            // ModelsBuilder generated models will expose their content type alias via the PublishedModelAttribute, so
+            // we can use that to check if the model is allowed - and whether the type actually has the attribute. If
+            // the attribute is missing, we just skip the type
+            string? modelAlias = type.GetCustomAttribute<PublishedModelAttribute>(false)?.ContentTypeAlias;
+            if (string.IsNullOrWhiteSpace(modelAlias)) continue;
+
+            // Models should also specify their item type (content, media, member) via the ModelItemType field. If the
+            // field is missing, we just skip the type
+            PublishedItemType itemType = type.GetField("ModelItemType", BindingFlags.Static | BindingFlags.Public)?.GetValue(null) as PublishedItemType? ?? PublishedItemType.Unknown;
+            if (itemType != objectType) continue;
+
+            // Legacy configuration of the MNTP data type would save the alias of the allowed content types in the
+            // Filter property. So we first check whether the model's alias is allowed
+            if (allowedTypes.Contains(modelAlias)) yield return type;
+
+            // If the model's alias is not allowed, we need to check by the key instead, in which case we need to look
+            // up the IPublishedContentType for the model. Umbraco is a bit silly here, as it will throw an exception
+            // if the content type alias is not found, but doesn't offer a way to check if the content type exists.
+            // TODO: should we wrap this in a try/catch?
+            if (_publishedContentTypeCache.Get(objectType, modelAlias) is not { } contentType) continue;
+
+            // If the model's content type key is allowed, we can return the type
+            if (allowedTypes.Contains($"{contentType.Key:D}")) yield return type;
+
+        }
 
     }
 
